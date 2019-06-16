361974b74d6ca31ef32f96ae758bab5bf5c42cd4
Mark <mark@remarkablemark.org>
2019-06-15 20:07:30 -0400

    test: add cases and compare parser output with `css.parse` output

---
+++ test/data.js
@@ -0,0 +1,49 @@
+const cases = [
+  // general
+  'display: inline-block;',
+  'color:red;',
+  'margin: 0 auto;',
+  'border: 5px solid #BADA55;',
+  'font-size: .75em; position:absolute;width: 33.3%; z-index:1337;',
+
+  // multiple of same property
+  'color:rgba(0,0,0,1);color:white;',
+
+  // missing semicolon
+  'line-height: 42',
+  'font-style:italic; text-transform:uppercase',
+
+  // extra whitespace
+  ' padding-bottom : 1px',
+  'padding:   12px  0 ',
+  `
+    -moz-border-radius: 10px 5px;
+    -webkit-border-top-left-radius: 10px;
+    -webkit-border-top-right-radius: 5px;
+    -webkit-border-bottom-right-radius: 10px;
+    -webkit-border-bottom-left-radius: 5px;
+    border-radius: 10px 5px;
+  `,
+
+  // text and url
+  'content: "Lorem ipsum";',
+  'background-image: url(http://example.com/img.png)',
+
+  // vendor prefixes
+  'background: -webkit-linear-gradient(90deg, black, #111)',
+  '-webkit-transition: all 4s ease; -moz-transition: all 4s ease; -ms-transition: all 4s ease; -o-transition: all 4s ease; transition: all 4s ease;',
+
+  // comment
+  '/* comment */',
+  'top: 0; /* comment */ bottom: 42rem;',
+  ` top: 0; /* comment */
+  /* comment */   bottom: 42rem; `,
+
+  // custom
+  'foo: bar;',
+  'foo:bar; baz:qux'
+];
+
+module.exports = {
+  cases
+};
--- test/index.js
+++ test/index.js
@@ -1,8 +1,36 @@
 const assert = require('assert');
+const css = require('css');
 const parse = require('..');
+const { cases } = require('./data');
 
-describe.skip('inline-style-parser', () => {
-  it('returns with placeholder', () => {
-    assert.equal(parse(), 'inline-style-parser');
+/**
+ * @param {string} inlineStyle
+ * @return {Array}
+ */
+function getDeclarations(inlineStyle) {
+  const {
+    stylesheet: { rules }
+  } = css.parse(`a{${inlineStyle}}`);
+  return rules[0].declarations;
+}
+
+/**
+ * @param {Object[]} declarations
+ * @return {Object[]}
+ */
+function removePosition(declarations) {
+  return declarations.map(({ type, property, value }) => ({
+    type,
+    property,
+    value
+  }));
+}
+
+cases.forEach(inlineStyle => {
+  it(`parses \`${inlineStyle}\``, () => {
+    assert.deepEqual(
+      removePosition(parse(inlineStyle)),
+      removePosition(getDeclarations(inlineStyle))
+    );
   });
 });
